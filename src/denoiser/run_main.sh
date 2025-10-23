ac006ecc10911c6f921a2a2b716f872065130610
shunpcho <kshuchi0203@gmail.com>
2025-10-23 20:19:03 +0900

    fix: run script

--- src/denoiser/run_main.sh
+++ src/denoiser/run_main.sh
@@ -1,20 +1,19 @@
-#  python src/denoiser/train.py \
-#  --train_data_path "/home/s.chochi/noise-translator/data/CC15" \
-#  --clean_img_keyword "_mean" \
-#  --noisy_img_keyword "_real" \
-#  --batch_size 4 \
-#  --cropsize 128 \
-#  --learning_rate 0.0001 \
-#  --iteration 30 \
-#  --interval 5 \
-#  --output_dir "./results" \
-#  --log_dir "logs" \
-#  --tensorboard True \
-#  --verbose "info"
+python src/denoiser/train.py \
+ --train_data_path "data/CC15" \
+ --clean_img_keyword "_mean" \
+ --noisy_img_keyword "_real" \
+ --batch_size 4 \
+ --cropsize 128 \
+ --learning_rate 0.0001 \
+ --iteration 20 \
+ --interval 5 \
+ --output_dir "./results" \
+ --log_dir "logs" \
+
 
 # For inference
-python src/denoiser/inference.py \
- --model "./results/best_model.pth" \
- --input "./data/CC15_inf" \
- --output "./results/inf"
+# python src/denoiser/inference.py \
+#  --model "./results/best_model.pth" \
+#  --input "./data/CC15_inf" \
+#  --output "./results/inf"
 
