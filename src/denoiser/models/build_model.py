a4200959b42f1e49d9df0cb6b3fc9b2cac0d409d
shunpcho <kshuchi0203@gmail.com>
2025-12-30 16:45:42 +0900

    feat: add model vit unet

--- src/denoiser/models/build_model.py
+++ src/denoiser/models/build_model.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 import torch
 from torch import nn
+from vit_unet.models.model import HViT_UNet
 
 from denoiser.models.unet import UNet
 
@@ -10,13 +11,21 @@ def create_model(
     model_name: str | None, in_channels: int = 3, out_channels: int | None = None, pretrained: bool = True
 ) -> nn.Module:
     """Create UNet model with standard parameters."""
-    if model_name == "vit_base_patch16_224":
-        return UNet(
-            encoder_name=model_name,
-            encoder_depth=4,
-            in_channels=in_channels,
-            out_channels=out_channels,
-            pretrained=pretrained,
+    if model_name == "vit_unet":
+        return HViT_UNet(
+            depth=2,
+            depth_te=2,
+            size_bottleneck=2,
+            preprocessing="conv",
+            im_size=256,
+            patch_size=32,
+            num_channels=3,
+            hidden_dim=128,
+            num_heads=8,
+            attn_drop=0.2,
+            proj_drop=0.2,
+            linear_drop=0,
+            verbose=False,
         )
     return UNet(encoder_name="resnet34", in_channels=in_channels, out_channels=out_channels, pretrained=pretrained)
 
