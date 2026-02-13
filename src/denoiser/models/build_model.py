1809f2bf5d49845218e3f15ff9a8d12dc1fa3494
shunpcho <kshuchi0203@gmail.com>
2026-02-13 23:06:36 +0900

    fix: vit-unet config

--- src/denoiser/models/build_model.py
+++ src/denoiser/models/build_model.py
@@ -2,7 +2,8 @@ from pathlib import Path
 
 import torch
 from torch import nn
-from vit_unet.models.model import HViT_UNet
+from vit_unet.config.model_config import VitunetConfig
+from vit_unet.models.model import ViTUNet
 
 from denoiser.models.unet import UNet
 
@@ -12,7 +13,7 @@ def create_model(
 ) -> nn.Module:
     """Create UNet model with standard parameters."""
     if model_name == "vit_unet":
-        return HViT_UNet(
+        config = VitunetConfig(
             depth=2,
             depth_te=2,
             size_bottleneck=2,
@@ -27,6 +28,7 @@ def create_model(
             linear_drop=0,
             verbose=False,
         )
+        return ViTUNet(config)
     return UNet(encoder_name="resnet34", in_channels=in_channels, out_channels=out_channels, pretrained=pretrained)
 
 
