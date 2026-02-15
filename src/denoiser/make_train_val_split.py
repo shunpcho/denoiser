65ee99521abab9596fc28d589dfa28b41aed9157
shunpcho <kshuchi0203@gmail.com>
2026-02-15 19:50:55 +0900

    feat: use train-val folder

--- src/denoiser/make_train_val_split.py
+++ src/denoiser/make_train_val_split.py
@@ -2,32 +2,47 @@ import argparse
 import json
 import random
 from pathlib import Path
+from typing import TypedDict
 
 from denoiser.configs.config import PairingKeyWords
 
 
+class TrainValSplitKwargs(TypedDict):
+    train_path: Path
+    val_path: Path
+
+
 def split_train_val(
     data_dir: Path,
-    train_ratio: float = 0.8,
+    train_val: TrainValSplitKwargs | float | None = None,
     img_read_keywords: PairingKeyWords | None = None,
 ) -> None:
     """Split dataset into training and validation sets and save the file lists as JSON files.
 
     Args:
         data_dir: Directory containing the images.
-        train_ratio: Ratio of training data to total data.
         img_read_keywords: Keywords to filter images based on filenames.
+        train_val: Optional paths for train and validation splits or a float for train ratio.
+
+    Raises:
+        TypeError: If train_val is not a TrainValSplitKwargs dict or a floats for train ratio.
     """
-    exts = [".png", ".jpg", ".jpeg", ".bmp", ".tif"]
-
-    files = [p for p in data_dir.rglob("*") if p.suffix.lower() in exts and _match_keywords(p, img_read_keywords)]
-
-    # Split into train and val
-    random.seed(42)
-    random.shuffle(files)
-    split_idx = int(len(files) * train_ratio)
-    train_files = files[:split_idx]
-    val_files = files[split_idx:]
+    if isinstance(train_val, dict):
+        train_data_dir = data_dir / train_val["train_path"]
+        val_data_dir = data_dir / train_val["val_path"]
+        train_files = _filter_files_by_keywords(train_data_dir, img_read_keywords)
+        val_files = _filter_files_by_keywords(val_data_dir, img_read_keywords)
+    elif isinstance(train_val, float):
+        files = _filter_files_by_keywords(data_dir, img_read_keywords)
+        # Split into train and val
+        random.seed(42)
+        random.shuffle(files)
+        split_idx = int(len(files) * train_val)
+        train_files = files[:split_idx]
+        val_files = files[split_idx:]
+    else:
+        msg = "train_val must be either a TrainValSplitKwargs dict or a float for train ratio."
+        raise TypeError(msg)
 
     # save to JSON
     out_dir = data_dir / "indices"
@@ -40,6 +55,11 @@ def split_train_val(
     print(f"train: {len(train_files)}, val: {len(val_files)}")
 
 
+def _filter_files_by_keywords(data_dir: Path, img_read_keywords: PairingKeyWords | None) -> list[Path]:
+    exts = {".png", ".jpg", ".jpeg", ".bmp", ".tif"}
+    return [p for p in data_dir.rglob("*") if p.suffix.lower() in exts and _match_keywords(p, img_read_keywords)]
+
+
 def _match_keywords(p: Path, keywords: PairingKeyWords | None) -> bool:
     if keywords is None:
         return True
@@ -49,7 +69,7 @@ def _match_keywords(p: Path, keywords: PairingKeyWords | None) -> bool:
     return keywords.clean in p.stem
 
 
-if __name__ == "__main__":
+def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument("--data-dir", type=Path, required=True, help="Path to the training data.")
     parser.add_argument(
@@ -61,11 +81,16 @@ if __name__ == "__main__":
     parser.add_argument(
         "--detector-keywords", type=str, nargs="*", default=None, help="List of detector keywords (optional)."
     )
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument(
+        "--train-val-path", type=Path, nargs=2, help="Paths to train and val if using directory-based split."
+    )
+    group.add_argument("--train-ratio", type=float, help="Ratio of training data (e.g., 0.8 for 80% train, 20% val).")
 
     args = parser.parse_args()
     args = vars(args)
 
-    pairing_keywords = (
+    args["pairing_keywords"] = (
         PairingKeyWords(
             clean=args.pop("clean_img_keyword"),
             noisy=args.pop("noisy_img_keyword"),
@@ -74,4 +99,16 @@ if __name__ == "__main__":
         if args.get("clean_img_keyword")
         else None
     )
-    split_train_val(**args, img_read_keywords=pairing_keywords)
+    if args.get("train_val_path"):
+        args["train_val"] = TrainValSplitKwargs(
+            train_path=args["train_val_path"][0],
+            val_path=args["train_val_path"][1],
+        )
+    elif args.get("train_ratio") is not None:
+        args["train_val"] = args.pop("train_ratio")
+
+    split_train_val(**args)
+
+
+if __name__ == "__main__":
+    main()
